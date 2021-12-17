43ba8435f4eb81b77b17ea3a703f4fd54d4faeec
Spiker <thespiker@ukr.net>
2021-12-17 20:14:05 +0200

    project ended

--- hw_FormApp/c_Form.h
+++ hw_FormApp/c_Form.h
@@ -1,5 +1,44 @@
+#include <iostream>
 #pragma once
 class c_Form
 {
+private:
+	char* name;
+	unsigned short age;
+public:
+
+	c_Form()
+	{
+
+	}
+	c_Form(char* name, unsigned short age)
+	{
+		this->name = new char[100];
+		strcpy_s(this->name, strlen(name) + 1, name);
+
+		this->age = age;
+	}
+
+	char* getName() {
+		return this->name;
+	}
+	void setName(char* name) {
+		this->name = new char[100];
+		strcpy_s(this->name, strlen(name) + 1, name);
+	}
+
+	unsigned short getAge() {
+		return age;
+	}
+	void setAge(unsigned short age)
+	{
+		this->age = age;
+	}
+
+	friend std::ostream& operator<<(std::ostream& out,c_Form form) {
+		out << "\nИмя: "<<form.getName();
+		out << "\tВозраст: " << form.getAge();
+		return out;
+	}
 };
 
--- hw_FormApp/hw_FormApp.cpp
+++ hw_FormApp/hw_FormApp.cpp
@@ -1,6 +1,31 @@
 ﻿#include <iostream>
+#include "c_Form.h"
 
+void showYoung(c_Form *funcForma) {
+	for (int i = 0; i < 10; i++) {
+		if (funcForma[i].getAge() < 18) {
+			std::cout << funcForma[i];
+		}
+	}
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	c_Form All_Forms[10];
+
+	unsigned short age;
+	for (int i = 0; i < 10; i++) {
+		char* TEMP = new char[100];
+
+		std::cout << "\nВведите имя: ";
+		gets_s(TEMP, 100);
+		All_Forms[i].setName(TEMP);
+		delete TEMP;
+		
+		std::cout << "Введите возраст: ";
+		std::cin >> age;
+		std::cin.ignore();
+		All_Forms[i].setAge(age);
+	}
+	showYoung(All_Forms);
 }
